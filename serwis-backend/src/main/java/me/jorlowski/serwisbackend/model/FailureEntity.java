8ba6fc61cc46a772e55003ca8335bdcda54f269e
JakiuDeus <jakub.krypno@gmail.com>
2024-04-14 22:54:03 +0200

    FailureEntity NotNull

--- serwis-backend/src/main/java/me/jorlowski/serwisbackend/model/FailureEntity.java
+++ serwis-backend/src/main/java/me/jorlowski/serwisbackend/model/FailureEntity.java
@@ -3,6 +3,7 @@ package me.jorlowski.serwisbackend.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,12 +20,19 @@ public class FailureEntity {
     @Id
     @GeneratedValue
     private Long id;
+    @NotNull
     private String servicerName;
+    @NotNull
     private FailureType failureType;
+    @NotNull
     private String name;
+    @NotNull
     private Date date;
+    @NotNull
     private Float potentialPrice;
+    @NotNull
     private Date potentialDate;
+    @NotNull
     private Status status;
     private String repairDescription;
 }
